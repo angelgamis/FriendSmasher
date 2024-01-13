6e7e1d05704bf24996fc779c5978e699d1cea8c2
angelgamis <angel28gamis@hotmail.com>
2024-01-12 20:11:10 -0600

    Creating bouncy ball

--- Assets/Scripts/BallMovement.cs
+++ Assets/Scripts/BallMovement.cs
@@ -1,18 +1,38 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class BallMovement : MonoBehaviour
 {
+    private Vector3 ballPosition;
+	private Rigidbody2D rigid;
+	// Give a random X force downwards
+	private float xForce;
+	private Vector2 force;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+		if(GetComponent<Rigidbody2D>() != null)
+		{
+			rigid = GetComponent<Rigidbody2D>();
+		}
+
+		xForce = Random.Range(-0.5f, 0.5f);
+		force = new Vector2(xForce, 1f);
+		ballPosition = transform.position;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
-    }
+		if(rigid != null)
+		{
+			rigid.AddForce(force);
+
+		}
+	}
+
+	private void OnCollisionEnter2D(Collision2D collision)
+	{
+		force *= -1;
+	}
 }
