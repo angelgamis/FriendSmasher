c8bdb8adda3b5888e87164d9d0476a5a5b00bdc8
angelgamis <angel28gamis@gmail.com>
2024-02-20 18:34:05 -0600

    Added code to brick to allow functionality of bricks. Bricks will get smaller and change sprite when at its final state

--- Assets/Scripts/Brick.cs
+++ Assets/Scripts/Brick.cs
@@ -3,6 +3,8 @@ using UnityEngine;
 public class Brick : MonoBehaviour
 {
     [SerializeField] private SpriteRenderer spriteRenderer;
+    [SerializeField] private float[] states;
+    [SerializeField] private Sprite endSprite;
     [SerializeField] private int health;
     public int Health => health;
 
@@ -13,7 +15,8 @@ public class Brick : MonoBehaviour
 	// Start is called before the first frame update
 	void Start()
     {
-        
+        health = states.Length;
+        gameObject.transform.localScale = new Vector2(states[health - 1], states[health - 1]);
     }
 
     // Update is called once per frame
@@ -21,4 +24,36 @@ public class Brick : MonoBehaviour
     {
         
     }
+
+	private void Hit()
+	{
+		// Lower health
+		health--;
+
+		// If health is depleted turn off object
+		if(health <= 0)
+		{
+			gameObject.SetActive(false);
+		}
+		// If health is at 1 change the sprite to end sprite
+		else if(health == 1)
+		{
+			spriteRenderer.sprite = endSprite;
+		}
+
+		// Update the scale of the object with new 
+		gameObject.transform.localScale = new Vector2(states[health - 1], states[health - 1]);
+	}
+
+	// When ball collides with brick call Hit function
+	private void OnCollisionEnter2D(Collision2D collision)
+	{
+		BallMovement ball = collision.gameObject.GetComponent<BallMovement>();
+
+		// If collision was the ball
+		if (ball != null)
+		{
+			Hit();
+		}
+	}
 }
